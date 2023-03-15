d6a4b1648335550264d7e02a80e1de6569107e97
Olivia Perez <op90210@gmail.com>
2023-03-15 01:54:02 -0400

    image load works properly

--- src/main/java/com/example/piratecertif/controllers/HomeController.java
+++ src/main/java/com/example/piratecertif/controllers/HomeController.java
@@ -1,14 +1,8 @@
 package com.example.piratecertif.controllers;
 
-import com.example.piratecertif.App;
 import io.github.palexdev.materialfx.controls.MFXButton;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-
-import java.io.IOException;
 
 public class HomeController {
     @FXML private MFXButton clickBT;
@@ -19,13 +13,14 @@ public class HomeController {
         clickBT.setOnMouseClicked(event -> {
             counter++;
             updateImage();
-            System.out.println(counter);
         });
     }
 
     public void updateImage(){
         imagesVB.getStyleClass().clear();
-        if(counter<5){imagesVB.getStyleClass().add("image-vbox1");}
-        else {imagesVB.getStyleClass().add("vbox");}
+        if(counter<5){imagesVB.getStyleClass().add("stage1");}
+        else if(counter<10){imagesVB.getStyleClass().add("stage2");}
+        else if(counter<15){imagesVB.getStyleClass().add("stage3");}
+        else {imagesVB.getStyleClass().add("stage4");}
     }
 }
