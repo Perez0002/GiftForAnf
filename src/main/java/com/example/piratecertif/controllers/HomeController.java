77c4117c5880ccffbc714f7bd8e0acf18986e9f0
Olivia Perez <op90210@gmail.com>
2023-03-17 14:40:55 -0400

    new levels fully implemented, new art needed

--- src/main/java/com/example/piratecertif/controllers/HomeController.java
+++ src/main/java/com/example/piratecertif/controllers/HomeController.java
@@ -16,14 +16,10 @@ public class HomeController {
     @FXML private Label instructionsLabel;
     private int counter = 0;
     private int currentLevel = 0;
+
     @FXML
     public void initialize(){
-        clickBT.setOnMouseClicked(event -> {
-            clickBT.setVisible(false);
-            clickBT.setPrefHeight(0);
-            instructionsLabel.setPrefHeight(60);
-            next();
-        });
+        next();
     }
 
     public void updateImage(int level){
@@ -34,14 +30,16 @@ public class HomeController {
 
     public void levelControl(){
         switch(currentLevel){
-            case 1 -> instructionsLabel.setText("Sailing! Steer left and right!");
-            case 2 -> instructionsLabel.setText("Fencing! Strike up, down, left, and right!");
-            case 3 -> instructionsLabel.setText("Pistol! Empty arrow = pull backwards to reload");
-            case 4 -> instructionsLabel.setText("Cannon! Empty arrow = pull backwards to reload");
-            case 5 -> instructionsLabel.setText("Finish your training!!!");
+            case 1 -> instructionsLabel.setText("Sailing! Steer left and right");
+            case 2 -> instructionsLabel.setText("Fencing! Strike up, down, left, and right");
+            case 3 -> instructionsLabel.setText("Pistol! Empty gun = pull 'backwards' to reload");
+            case 4 -> instructionsLabel.setText("Cannons! Empty cannon = pull 'backwards' to reload");
+            case 5 -> instructionsLabel.setText("Lightning! Get to a safe 'space'");
+            case 6 -> instructionsLabel.setText("Repair! 'Enter' below deck to fix the damage");
         }
 
         int num = (int)(Math.random() * ((currentLevel*2) - 1 + 1)) + 1;
+        //System.out.println(num);
         switch (num) {
             case 1 -> arrowFontIcon.setIconLiteral("typ-arrow-right-thick");
             case 2 -> arrowFontIcon.setIconLiteral("typ-arrow-left-thick");
@@ -51,14 +49,15 @@ public class HomeController {
             case 6 -> arrowFontIcon.setIconLiteral("typ-arrow-left-outline");
             case 7 -> arrowFontIcon.setIconLiteral("typ-arrow-up-outline");
             case 8 -> arrowFontIcon.setIconLiteral("typ-arrow-down-outline");
-            default-> clickBT.setText("error");
+            case 9, 10 -> arrowFontIcon.setIconLiteral("typ-flash-outline");
+            case 11, 12 ->  arrowFontIcon.setIconLiteral("typ-spanner");
+            default-> instructionsLabel.setText("error");
         }
         check();
     }
 
-
     public void check() {
-        clickBT.getScene().setOnKeyPressed(event -> {
+        clickBT.setOnKeyPressed(event -> {
             if ((arrowFontIcon.getIconLiteral().equals("typ-arrow-right-thick")
                     ||  arrowFontIcon.getIconLiteral().equals("typ-arrow-left-outline"))
                     && event.getCode() == KeyCode.RIGHT){counter++;}
@@ -71,17 +70,23 @@ public class HomeController {
             else if ((arrowFontIcon.getIconLiteral().equals("typ-arrow-down-thick")
                     || arrowFontIcon.getIconLiteral().equals("typ-arrow-down-outline"))
                     && event.getCode() == KeyCode.DOWN) {counter++;}
+            else if (arrowFontIcon.getIconLiteral().equals("typ-flash-outline")
+                    && event.getCode() == KeyCode.SPACE) {counter++;}
+            else if (arrowFontIcon.getIconLiteral().equals("typ-spanner")
+                    && event.getCode() == KeyCode.ENTER) {counter++;}
             else {counter--;}
             System.out.println(counter);
             next();
         });
     }
+
     public void next() {
         if(counter<5){currentLevel=1; updateImage(1); levelControl();}
         else if(counter<10){currentLevel=2; updateImage(2); levelControl();}
         else if(counter<15){currentLevel=3; updateImage(3); levelControl();}
         else if(counter<20){currentLevel=4; updateImage(4); levelControl();}
-        else if(counter<30){currentLevel=5; updateImage(5); levelControl();}
+        else if(counter<25){currentLevel=5; updateImage(5); levelControl();}
+        else if(counter<35){currentLevel=6; updateImage(6); levelControl();}
         else{Navigation.navigate(Screen.CERTIF);}
     }
 }
